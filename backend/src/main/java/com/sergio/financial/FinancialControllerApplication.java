d3c232c67c5aa4ec7f87485309510dd41ac1af53
sergiomansoo <sergio.manso2007@gmail.com>
2026-08-12 20:20:54 -0300

    chore(backend): scaffold Spring Boot application

---
+++ backend/src/main/java/com/sergio/financial/FinancialControllerApplication.java
@@ -0,0 +1,12 @@
+package com.sergio.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialControllerApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/sergio/financial/FinancialControllerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sergio.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
